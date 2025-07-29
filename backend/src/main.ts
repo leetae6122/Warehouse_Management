e77434a3e822b65529ba7bb9c7428056e7f4a937
Le Duong Tri <102638455+leetae6122@users.noreply.github.com>
2025-07-29 22:03:16 +0700

    add cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Warehouse Management - CRUD')
     .setDescription('NestJS - Prisma - Postgresql')
